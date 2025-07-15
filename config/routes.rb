02fc94235f6363cb7823de45ae76ae714ad645ad
Kenneth Kebaara <kkebaara@yahoo.com>
2025-07-15 17:00:44 -0600

    added test route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/api", to: proc { [200, {}, ["API is working!"]] }
   get "/api/tutors/:tutor_id/students", to: "students#index"
   patch "/api/students/:id/lesson_complete", to: "students#mark_complete"
 end
